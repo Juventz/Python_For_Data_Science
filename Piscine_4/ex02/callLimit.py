800fc54037e1a689b64d6a194c718260d4ead6bf
Juventz ARISTIL MATHURIN <jaristil@made-f0Cr8s2.clusters.42paris.fr>
2024-10-03 19:53:04 +0200

    have to finish from main

--- Piscine_4/ex02/callLimit.py
+++ Piscine_4/ex02/callLimit.py
@@ -14,8 +14,21 @@ def callLimit(limit: int):
                 count += 1
                 return function(*args, **kwargs)
             else:
-                raise Exception(f"Error: {function} call too many times")
+                raise Exception(Fore.RED + f"Error: {function} call\
+too many times")
         return limit_function
     return callLimiter
 
 
+def main():
+    @callLimit(3)
+    def f():
+        print("f()")
+
+    @callLimit(1)
+    def g():
+        print("g()")
+
+    for i in range(3):
+        f()
+    g()
