9136198d08a42a74d019be3b83ede1f11865a6f4
Juventz ARISTIL MATHURIN <jaristil@paul-f4Br3s4.clusters.42paris.fr>
2024-10-08 17:43:45 +0200

    piscine_4 tested and finishedm have to submit

--- Piscine_4/ex03/new_student.py
+++ Piscine_4/ex03/new_student.py
@@ -4,16 +4,47 @@ from string import ascii_lowercase
 
 
 def generate_id() -> str:
+    """Generate a random string of 15 lowercase letters."""
     return "".join(choices(ascii_lowercase, k=15))
 
 
 @dataclass
 class Student:
+    """A class representing a student."""
     name: str
     surname: str
     active: bool = True
     login: str = field(init=False)
     id: str = field(init=False, default_factory=generate_id)
 
-    def __post_init__(self):
-        self.login = f"{self.name[0]}{self.surname}".capitalize()
+    def __init__(self, name: str, surname: str, active: bool = True, **kwargs):
+        try:
+            if 'id' in kwargs:
+                raise TypeError("Student.__init__() got an unexpected\
+keyword argument 'id'")
+
+            self.name = name
+            self.surname = surname
+            self.active = active
+            self.login = f"{self.name[0]}{self.surname}".capitalize()
+
+        except Exception as e:
+            print(f"{type(e).__name__}: {e}")
+            exit(1)
+
+
+def main():
+    print(Student.__doc__)
+    print(generate_id.__doc__)
+    print("")
+    try:
+        student = Student(name="Edward", surname="agle", id="toto")
+        print(student)
+
+    except Exception as e:
+        print(f"{type(e).__name__}: {e}")
+        exit(1)
+
+
+if __name__ == "__main__":
+    main()
